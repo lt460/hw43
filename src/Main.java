e9c0bceeb7d18a9f628da73e39abe609e642f1f4
lt460 <130529289+lt460@users.noreply.github.com>
2023-06-16 17:11:30 +0600

    Добавлены 1 отдельный обработчик -  handleAppsRequest

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.Writer;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.nio.charset.Charset;
@@ -15,14 +12,12 @@ public class Main {
     public static void main(String[] args) {
         try {
             HttpServer server = makeServer();
+            initRoutes(server);
             server.start();
-
         } catch (IOException e) {
             e.printStackTrace();
-
         }
     }
-
     private static HttpServer makeServer() throws IOException {
         String host = "localhost";
         InetSocketAddress address = new InetSocketAddress(host, 8089);
@@ -33,13 +28,34 @@ public class Main {
         return server;
 
     }
-
     private static void initRoutes(HttpServer server) {
         server.createContext("/", Main::handleRequest);
-        server.createContext("/apps/", Main::handleRequest);
+        server.createContext("/apps/", Main::handleAppsRequest);
         server.createContext("/apps/profile", Main::handleRequest);
 
     }
+    private static void handleAppsRequest(HttpExchange exchange) {
+        try {
+            exchange.getResponseHeaders().add("Content-Type", "text/plain; charset=utf-8");
+            int responseCode = 200;
+            int length = 0;
+            exchange.sendResponseHeaders(responseCode, length);
+            try (PrintWriter writer = getWriterFrom(exchange)) {
+                String method = exchange.getRequestMethod();
+                URI uri = exchange.getRequestURI();
+                String ctxPath = exchange.getHttpContext().getPath();
+                write(writer, "HTTP Метод", method);
+                write(writer, "Запрос", uri.toString());
+                write(writer, "Обработан через", ctxPath);
+                writeHeaders(writer, "Заголовки запроса", exchange.getRequestHeaders());
+                write(writer, "папка APPS", "Это страница веб-приложений.");
+                writer.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private static void handleRequest(HttpExchange exchange) {
 
         try {
@@ -63,15 +79,11 @@ public class Main {
         }
 
     }
-
     private static PrintWriter getWriterFrom(HttpExchange exchange) {
         OutputStream output = exchange.getResponseBody();
         Charset charset = StandardCharsets.UTF_8;
         return new PrintWriter(output, false, charset);
     }
-
-
-
     private static void write(Writer writer, String msg, String method) {
         String data = String.format("%s: %s%n%n", msg, method);
         try {
@@ -85,4 +97,23 @@ public class Main {
         headers.forEach((k, v) -> write(writer, "\t" + k, v.toString()));
     }
 
+    private static BufferedReader getReader(HttpExchange exchange) {
+        InputStream input = exchange.getRequestBody();
+        Charset charset = StandardCharsets.UTF_8;
+        InputStreamReader isr = new InputStreamReader(input, charset);
+        return new BufferedReader(isr);
+    }
+
+    private static void writeData(Writer writer, HttpExchange exchange) {
+        try (BufferedReader reader = getReader(exchange)) {
+            if (!reader.ready()) {
+                return;
+            }
+            write(writer, "Блок данных", "");
+            reader.lines().forEach(v -> write(writer, "\t", v));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
